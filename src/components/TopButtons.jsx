e59935344689b57e8f64c32ba70fa74577992421
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-07-03 13:12:20 +0200

    added the functionality to the top buttons

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 
-function TopButtons() {
-  return <div>TopButtons</div>;
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: 'London',
+    },
+    {
+      id: 2,
+      title: 'Sydney',
+    },
+    {
+      id: 3,
+      title: 'Tokyo',
+    },
+    {
+      id: 4,
+      title: 'Toronto',
+    },
+    {
+      id: 5,
+      title: 'Paris',
+    },
+    {
+      id: 6,
+      title: 'Lagos',
+    },
+    {
+      id: 7,
+      title: 'Argentina',
+    },
+    {
+      id: 8,
+      title: 'Suriname',
+    },
+  ];
+
+  return (
+    <div className='flex items-center justify-around my-6'>
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className='text-white text-lg font-medium'
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default TopButtons;
